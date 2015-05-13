93059c9a4c28c85f7978ae7ec25267fccf55e8ac
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-05-13 07:46:05 -0500

    A lone spec

--- spec/models/tree_spec.rb
+++ spec/models/tree_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Tree, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.name' do
+    let(:tree1) { FactoryGirl.build(:tree, name: 'Test') }
+    let(:tree2) { FactoryGirl.build(:tree) }
+
+    it 'must be unique' do
+      tree1.save
+      tree2.name = 'Test'
+      tree2.save
+      expect(tree2).to_not be_valid
+    end
+  end
 end
